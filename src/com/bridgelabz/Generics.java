81233dc943ce83b0679d4de2f7e84f030c9880bd
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-04 15:59:02 +0530

    Added:code for Largest Integer

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -3,8 +3,20 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class Generics {
-    public static void main(String[] args){
-        System.out.println("Welcome to generics...");
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+        T max = x;
+
+        if (y.compareTo(max) > 0)
+            max = y;
+
+        if (z.compareTo(max) > 0)
+            max = z;
+
+        return max;
     }
-    
+
+    public static void main(String args[]) {
+        System.out.println("Maximum of int "+ maximum(3, 4, 5));//Added for Integer
+
+    }
 }
