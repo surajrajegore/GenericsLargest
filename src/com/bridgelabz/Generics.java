cc9416e9ca9626cef592dc43f2281269aef2dc3b
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-04 15:21:31 +0530

    Added:code of welcome message on master

---
+++ src/com/bridgelabz/Generics.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Generics {
+    public static void main(String[] args){
+        System.out.println("Welcome to generics...");
+    }
+    
+}
